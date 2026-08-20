ee3c0533308ff9e387393b70a510a9d8886bd291
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:12:05 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-rfc-2046",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(
@@ -21,10 +21,22 @@ let package = Package(
         .package(url: "https://github.com/swift-ietf/swift-rfc-2183.git", branch: "main"),
         .package(url: "https://github.com/swift-ietf/swift-rfc-4648.git", branch: "main"),
         .package(url: "https://github.com/swift-ietf/swift-rfc-5322.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-byte-collection-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-parser-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-byte-parser-primitives.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-byte-collection-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-parser-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-byte-parser-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -34,10 +46,16 @@ let package = Package(
                 .product(name: "RFC 2183", package: "swift-rfc-2183"),
                 .product(name: "RFC 4648", package: "swift-rfc-4648"),
                 .product(name: "RFC 5322", package: "swift-rfc-5322"),
-                .product(name: "Byte Collection Primitives", package: "swift-byte-collection-primitives"),
-                .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives"),
+                .product(
+                    name: "Byte Collection Primitives",
+                    package: "swift-byte-collection-primitives"
+                ),
+                .product(
+                    name: "Parseable ASCII Primitives",
+                    package: "swift-ascii-parser-primitives"
+                ),
                 .product(name: "Parser Primitives", package: "swift-parser-primitives"),
-                .product(name: "Byte Parser Primitives", package: "swift-byte-parser-primitives")
+                .product(name: "Byte Parser Primitives", package: "swift-byte-parser-primitives"),
             ]
         ),
         .testTarget(
